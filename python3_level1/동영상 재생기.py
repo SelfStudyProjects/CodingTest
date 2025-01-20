5aaf0908b4ce9a489a3b9e0d343dbbb906e6110b
najasinis <145651124+najasinis@users.noreply.github.com>
2025-01-21 02:00:02 +0900

    update file

--- python3_level1/동영상 재생기.py
+++ python3_level1/동영상 재생기.py
@@ -1,3 +1,8 @@
+# 써야 하는 함수 :
+# 정의내려야 하는 것들(def) : time_to_seconds, seconds_to_time, solution
+# 구현해야 하는 기능들 : commands 한개씩 분할(for in), prev(-=, if문 10초 미만), next(+=, if문 video_len 초과), if문 활용(op_start ≤ 현재 재생 위치 ≤ op_end -> op_end)
+
+
 def time_to_seconds(time):
     minutes, seconds = map(int, time.split(':'))
     return minutes * 60 + seconds
@@ -14,19 +19,20 @@ def solution(video_len, pos, op_start, op_end, commands):
     opening_end = time_to_seconds(op_end)
 
     for command in commands:
-
+        
         if opening_start <= current_pos <= opening_end:
             current_pos = opening_end
-
+        
         if command == "prev":
             current_pos -= 10
             if current_pos < 0:
                 current_pos = 0
+                
         elif command == "next":
             current_pos += 10
             if current_pos > video_length:
                 current_pos = video_length
-
+        
     if opening_start <= current_pos <= opening_end:
         current_pos = opening_end
 
