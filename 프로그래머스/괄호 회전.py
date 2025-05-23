3b33a1ce49bba2cf78e930a0c15224d81dc94940
najasinis <145651124+najasinis@users.noreply.github.com>
2025-05-23 23:03:55 +0900

    update file

--- 프로그래머스/괄호 회전.py
+++ 프로그래머스/괄호 회전.py
@@ -4,25 +4,31 @@
 #     return answer
 
 def solution(s):
-    answer = -1 # 왜 0이 아니라 -1일까?
+    answer = -1
     answer += 1
-    stack = []
-    num = len(s) # 원소의 총 개수 세기
-    for i in range(num) : # for 반복문(num 수만큼 반복하기)
-        answer += 1
-        t = s[i:] + s[:i-1]
+    num = len(s) # 문자열 길이
+    
+    # 괄호 짝을 딕셔너리로 정의!
+    bracket_pairs = {'[': ']', '(': ')', '{': '}'}
 
-    # (), {}, [] 종류 구분하기
-    # 아래 메커니즘에 따라서 각 종류마다 모두 해당되는 경우가 있는지 체킹
-
-    for char in s:
-        if char in ['[', '(', '{']:
-            stack.append(char)
-        elif char in [']', ')', '}']:
-            if stack:
-                stack.pop()
-            else:
-                return False
-
-    return len(stack) == 0
+    # 문자열을 num번 회전시키는 반복문 시작!
+    for i in range(num):
+        stack = [] # 스택 초기화! (매 회전마다 스택은 비어있어야 함!)
+        rotated_s = s[i:] + s[:i] # i번째 회전된 문자열 만들기!
+        
+        # rotated_s의 각 문자에 대해 괄호 체크!
+        for char in rotated_s:
+            if char in ['[', '(', '{']: # 여는 괄호면 스택에 push!
+                stack.append(char)
+            elif char in [']', ')', '}']: # 닫는 괄호면?
+                if stack: # 스택이 비어있지 않으면?
+                    top = stack.pop() # 스택의 가장 위
+                    if bracket_pairs[top] != char :
+                        stack = None
+                        break
+                else:
+                    stack = None
+                    break
+        if stack == [] :
+            answer += 1
     return answer
