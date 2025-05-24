96dc476d01537478b0c98b811ed62bc08fb2971a
najasinis <145651124+najasinis@users.noreply.github.com>
2025-05-24 12:07:02 +0900

    Update 괄호 회전.py

--- 프로그래머스/괄호 회전.py
+++ 프로그래머스/괄호 회전.py
@@ -13,15 +13,15 @@ def solution(s):
 
     # 문자열을 num번 회전시키는 반복문 시작!
     for i in range(num):
-        stack = [] # 스택 초기화! (매 회전마다 스택은 비어있어야 함!)
+        stack = [] # 스택 초기화 (매 회전마다 스택은 비어있어야 함)
         rotated_s = s[i:] + s[:i] # i번째 회전된 문자열 만들기!
         
-        # rotated_s의 각 문자에 대해 괄호 체크!
+        # rotated_s의 각 문자에 대해 괄호 체크
         for char in rotated_s:
-            if char in ['[', '(', '{']: # 여는 괄호면 스택에 push!
+            if char in ['[', '(', '{']: # 여는 괄호면 스택에 push
                 stack.append(char)
-            elif char in [']', ')', '}']: # 닫는 괄호면?
-                if stack: # 스택이 비어있지 않으면?
+            elif char in [']', ')', '}']: # 닫는 괄호
+                if stack: # 스택이 비어있지 않으면
                     top = stack.pop() # 스택의 가장 위
                     if bracket_pairs[top] != char :
                         stack = None
