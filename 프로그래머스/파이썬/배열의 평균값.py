637aa217fa41bfaa32f7bb2df73d0ddb9ad55bd1
najasinis <145651124+najasinis@users.noreply.github.com>
2025-08-19 13:27:38 +0900

    update file

--- 프로그래머스/파이썬/배열의 평균값.py
+++ 프로그래머스/파이썬/배열의 평균값.py
@@ -3,3 +3,13 @@ import numpy as np
 def solution(numbers):
     answer = np.sum(numbers) / len(numbers)
     return answer
+
+# 위의 것도 오키, 그리고 아래 것도 오키
+
+def solution(numbers):
+    answer = 0
+    sum = 0
+    for i in numbers :
+        sum += i
+    answer = sum / len(numbers)
+    return answer
