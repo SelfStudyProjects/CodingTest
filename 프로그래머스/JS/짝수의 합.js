99afe3ec79ecb6fe039ea69778392ed7577ee0dd
najasinis <145651124+najasinis@users.noreply.github.com>
2025-08-21 10:47:59 +0900

    update file

---
+++ 프로그래머스/JS/짝수의 합.js
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120831?language=javascript
+
+function solution(n) {
+    var answer = 0;
+    for (let i = 2; i <= n; i+=2){
+        answer += i
+    }
+    return answer;
+}
