7e973ab27f5e6bfb828d41c7ccb8f9870665bc87
najasinis <145651124+najasinis@users.noreply.github.com>
2025-09-05 08:50:48 +0900

    배열의 유사도 파일 수정

--- 프로그래머스/파이썬/배열의 유사도.py
+++ 프로그래머스/파이썬/배열의 유사도.py
@@ -7,3 +7,19 @@ def solution(s1, s2):
             if s1[i] == s2[j]:
                    answer += 1
     return answer;
+
+'''
+js 코드로 작성(동일한 로직)
+    function solution(s1, s2) {
+    var answer = 0;
+    for(i = 0; i < s1.length; i++) {
+        for(j = 0; j < s2.length; j++) {
+            if(s1[i] === s2[j]) {
+                answer += 1
+            }
+        }
+    }
+    return answer;
+}
+
+'''
