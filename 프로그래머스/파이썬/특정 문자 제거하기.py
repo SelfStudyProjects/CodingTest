258879474f5575ffe98647fc76e855b7fdba4baa
najasinis <145651124+najasinis@users.noreply.github.com>
2025-09-04 21:53:04 +0900

    특정 문자 제거하기 추가

--- 프로그래머스/파이썬/특정 문자 제거하기.py
+++ 프로그래머스/파이썬/특정 문자 제거하기.py
@@ -1,2 +1,13 @@
+# 내 풀이
+
 def solution(my_string, letter):
     return my_string.replace(letter, '')
+
+# 다른 사람 풀이
+
+def solution(my_string, letter):
+    answer = ''
+    for string in my_string:
+        if string != letter:
+            answer += string
+    return answer
