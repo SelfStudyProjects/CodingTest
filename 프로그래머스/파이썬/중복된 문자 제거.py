72de53971b274304763141d3f4795cbeebb93c39
najasinis <145651124+najasinis@users.noreply.github.com>
2025-10-02 04:31:32 +0900

    중복된 문자 제거 파일 수정

--- 프로그래머스/파이썬/중복된 문자 제거.py
+++ 프로그래머스/파이썬/중복된 문자 제거.py
@@ -2,3 +2,14 @@ def solution(my_string):
     return ''.join(dict.fromkeys(my_string))
 
 solution('people')
+
+'''
+아래 풀이도 참고!
+'''
+
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i not in answer:
+            answer+=i
+    return answer
