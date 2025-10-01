6a886f9ea227304012b5efbb61a9072d81081f89
najasinis <145651124+najasinis@users.noreply.github.com>
2025-10-02 05:45:06 +0900

    A로 B 만들기 파일 수정

---
+++ 프로그래머스/파이썬/A로 B 만들기.py
@@ -0,0 +1,29 @@
+def solution(before, after):
+    # 두 문자열을 각각 정렬해서 비교.
+    # 애너그램이라면 정렬했을 때 완전히 같은 문자열이 됨.
+    if sorted(before) == sorted(after):
+        return 1
+    else:
+        return 0
+    
+    # 한 줄로도 가능: return 1 if sorted(before) == sorted(after) else 0
+
+'''
+아래 내용도 괜찮음
+'''
+
+def solution(before, after):
+    
+    after_list = list(after) 
+    
+    for char_b in before:
+        if char_b in after_list:
+            after_list.remove(char_b)
+        else:
+            return 0
+            
+    # 아무것도 없을 때 false, 있다면 true
+    if not after_list: # after_list가 비어있으면 True
+        return 1
+    else: # after_list가 비어있지 않으면 False
+        return 0
