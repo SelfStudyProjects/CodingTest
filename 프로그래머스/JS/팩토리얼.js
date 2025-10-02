f9d953752a3c05bc9e8666069269ce7b78df0a4d
najasinis <145651124+najasinis@users.noreply.github.com>
2025-10-03 05:54:36 +0900

    팩토리얼.js 파일 생성

---
+++ 프로그래머스/JS/팩토리얼.js
@@ -0,0 +1,15 @@
+/* 숫자 n이 있다고 하자. n을 1부터 ++1을 하는 와중에, n!를 하자.
+그리고 처음으로 n보다 클 때 바로 return 0를 해서 멈추게끔 하자. */
+
+function solution(n) {
+    let i = 1;
+    let factorial = 1;
+
+    while (true) { 
+        factorial *= i;
+        if (factorial > n) {
+            return i - 1; 
+        }
+        i++;
+    }
+}
