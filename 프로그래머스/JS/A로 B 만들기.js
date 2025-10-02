bcb3e5a005a1dc66c6c3a2bf1c3e2d1db5d7d1e7
najasinis <145651124+najasinis@users.noreply.github.com>
2025-10-03 00:07:05 +0900

    A로 B 만들기.js 파일 수정

--- 프로그래머스/JS/A로 B 만들기.js
+++ 프로그래머스/JS/A로 B 만들기.js
@@ -1,3 +1,10 @@
+/*
+splice() 메소드의 기본적인 형태는 배열.splice(startIndex, deleteCount, item1, item2, ...) 이렇게 돼.
+startIndex: 작업을 시작할 배열의 인덱스.
+deleteCount: startIndex부터 몇 개의 요소를 제거할지 지정. (0이면 제거하지 않음)
+item1, item2, ...: (선택 사항) 제거된 요소 자리에 추가할 요소들.
+*/
+
 function solution(before, after) {
     answer = 0;
 
