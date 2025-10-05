0ddb09f3aa6a15e8873744059a7cf61839fad2b5
najasinis <145651124+najasinis@users.noreply.github.com>
2025-10-05 09:06:31 +0900

    한 번만 등장한 문자.py 수정

--- 프로그래머스/파이썬/한 번만 등장한 문자.py
+++ 프로그래머스/파이썬/한 번만 등장한 문자.py
@@ -16,3 +16,14 @@ def solution(s):
     #    만약 unique_chars_list가 비어있으면, ''.join([])는 빈 문자열 ''을 반환하므로 문제의 조건에 맞음.
     #    정렬된 문자들을 하나의 문자열로 합쳐서 반환!
     #    만약 unique_chars_list가 비어있으면, ''.join([])는 빈 문자열 ''을 반환하므로 문제의 조건에 맞음.
+
+    
+# Counter() 메소드를 안써도 되어서 훨씬 심플
+def solution(s):
+    answer = ''
+    list_s = list(s)
+    list_s.sort()
+    for i in list_s:
+        if list_s.count(i)==1:
+            answer+=i
+    return answer
