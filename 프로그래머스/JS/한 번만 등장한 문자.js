25184104263af3209a09f67eceb4b3e9c4ee7043
najasinis <145651124+najasinis@users.noreply.github.com>
2025-10-10 01:59:10 +0900

    가까운 수.py 파일 생성

---
+++ 프로그래머스/JS/한 번만 등장한 문자.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    const charCounts = {}; 
+    for (const char of s) {
+        charCounts[char] = (charCounts[char] || 0) + 1;
+
+    }
+
+    const uniqueCharsList = []; 
+        for (const char of s) { 
+        if (charCounts[char] === 1) { 
+            uniqueCharsList.push(char); 
+            }
+        }
+    
+    uniqueCharsList.sort(); 
+    
+    return uniqueCharsList.join(''); 
+}
