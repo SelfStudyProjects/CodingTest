69a91a982d46cae2c519977048ff14d0f84db7d8
najasinis <145651124+najasinis@users.noreply.github.com>
2025-10-28 23:09:56 +0900

    8이라는 숫자가 총 몇번 생성 수정

--- 프로그래머스/파이썬/8이라는 숫자가 총 몇번.py
+++ 프로그래머스/파이썬/8이라는 숫자가 총 몇번.py
@@ -1,5 +1,9 @@
 <img width="805" height="142" alt="Image" src="https://github.com/user-attachments/assets/8dc65dba-7f8f-4e4b-b3d6-889cdb7f8eb0" />
 
-# for 반복문 및 range(1001) 메소드들을 활용할 예정
-# 숫자를 배열로 바꾸고 그것들을 한 요소 별로 나누어서 체크
-# 하는 로직으로 가보자. While(True) 같은 걸로 각 배열의 모든 요소들을 검증해보고, 거기서 8이 나오면 count += 1을 하는 형태로 가보자 
+def count_eights():
+    answer = 0
+    for number in range(1, 10001):
+        answer += str(number).count('8')
+    return answer
+
+print(count_eights())
