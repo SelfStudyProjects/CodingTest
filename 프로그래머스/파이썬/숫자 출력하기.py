24cea085a982c93d1cc1586e49666a63555e7236
najasinis <145651124+najasinis@users.noreply.github.com>
2025-11-02 02:55:32 +0900

    숫자 출력하기 수정

--- 프로그래머스/파이썬/숫자 출력하기.py
+++ 프로그래머스/파이썬/숫자 출력하기.py
@@ -1,4 +1,8 @@
 # 20150111을 출력하는 함수
+# 아스키 코드 기준, a는 97이고 A는 65이다.
+# ord() 함수는 문자의 아스키 코드 값을 반환
+# 
+
 def print_magic_number():
     # 1. 아스키 코드를 이용해 필요한 숫자(0, 1, 2, 5)를 문자열로 생성
 
@@ -10,7 +14,7 @@ def print_magic_number():
     two = str(ord('c') - ord('a'))
     # '5' 만들기: ord('f') - ord('a') 결과는 숫자 5. 이를 str()로 문자열 '5'로 변환.
     five = str(ord('f') - ord('a'))
-
+    
     # 2. 생성된 숫자 문자열들을 순서대로 이어 붙여 최종 문자열 생성
     result_string = two + zero + one + five + zero + one + one + one
     
