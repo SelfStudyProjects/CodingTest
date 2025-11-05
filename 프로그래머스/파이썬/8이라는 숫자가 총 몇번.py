0a87a938eed821d4a562f5a847561cae50d2e86b
najasinis <145651124+najasinis@users.noreply.github.com>
2025-11-05 17:51:45 +0900

    8이라는 숫자가 총 몇 번 수정

--- 프로그래머스/파이썬/8이라는 숫자가 총 몇번.py
+++ 프로그래머스/파이썬/8이라는 숫자가 총 몇번.py
@@ -1,5 +1,3 @@
-<img width="805" height="142" alt="Image" src="https://github.com/user-attachments/assets/8dc65dba-7f8f-4e4b-b3d6-889cdb7f8eb0" />
-
 def count_eights():
     answer = 0
     for number in range(1, 10001):
