1359863600555b697382a98ec6963a845ec75a7a
najasinis <145651124+najasinis@users.noreply.github.com>
2025-11-15 17:11:21 +0900

    feat: 연결 리스트 수정

--- 코테 실습/파이썬/연결 리스트.py
+++ 코테 실습/파이썬/연결 리스트.py
@@ -1,3 +1,16 @@
+'''
+문제 설명
+단일 연결 리스트의 특정 노드 뒤에 새 노드를 삽입하는 함수를 작성하세요.
+주어진 노드 node의 바로 다음 위치에 값 new_val을 가지는 새 노드를 삽입해야 합니다.
+
+print(curr.val, end=' -> ' if curr.next else '\n')
+동작: print 함수를 사용하여 현재 curr가 가리키는 노드의 val 속성(값)을 출력합니다.
+핵심: end 매개변수를 사용해서 출력 방식(1 -> 2 -> 3)을 조절합니다.
+if curr.next else '\n': 조건부 표현식입니다.
+만약 curr.next가 None이 아니면 (즉, 다음 노드가 존재하면, True), 출력 끝에 ' -> ' 문자열을 붙입니다.
+만약 curr.next가 None(False)이면 (마지막 노드이면), 출력 끝에 줄 바꿈 문자 \n을 붙여 다음 줄로 이동시킵니다.
+'''
+
 class Node:
     def __init__(self, val):
         self.val = val
